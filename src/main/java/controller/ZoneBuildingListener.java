f0b44d7cc8dbf75cd3613bfc86adf1b66249e5f5
Zhang Li <2590945318@qq.com>
2023-03-25 17:11:58 +0100

    Base controller stuffs.

---
+++ src/main/java/controller/ZoneBuildingListener.java
@@ -0,0 +1,21 @@
+package controller;
+
+import controller.util.GameMode;
+import controller.util.Property;
+import model.util.Coordinate;
+
+public class ZoneBuildingListener extends ServiceListener{
+
+
+    public ZoneBuildingListener(Property property) {
+        super(property);
+    }
+
+    @Override
+    public void update(Coordinate coordinate) {
+        // TODO create instance of zone according to game mode
+        GameMode gmo = property.getGameMode();
+        System.out.println(gmo);
+        // TODO add zone to game model.
+    }
+}
