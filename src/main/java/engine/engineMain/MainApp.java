f6a5434205febbfda334d975b00643bb913f03c8
Abdelaziz Dhaouadi <azizdh0102@gmail.com>
2023-03-28 15:46:03 +0200

    add model and loader class

--- src/main/java/engine/engineMain/MainApp.java
+++ src/main/java/engine/engineMain/MainApp.java
@@ -1,15 +1,46 @@
 package engine.engineMain;
 
 import engine.display.DisplayManager;
+import engine.engineMain.models.RawModel;
+import engine.engineMain.renderEngine.Loader;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
 import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+
 public class MainApp {
 
     public static void main(String[] args) {
         DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f,0f,
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f
+
+        };
+
+        int[] indices = {
+                0,1,3,
+                3,1,2
+        };
+        RawModel model = loader.loadToVAO(vertices,indices);
 
         while(!glfwWindowShouldClose(DisplayManager.window)) {
+            GL11.glClearColor(1,0,0,1);
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+            GL30.glBindVertexArray(model.getVaoID());
+            GL20.glEnableVertexAttribArray(0);
+            GL11.glDrawArrays(GL11.GL_TRIANGLES,0,model.getVertexCount());
+            GL20.glDisableVertexAttribArray(0);
             DisplayManager.updateDisplay();
+
         }
+        loader.cleanUp();
         DisplayManager.closeDisplay();
     }
+
+
 }
