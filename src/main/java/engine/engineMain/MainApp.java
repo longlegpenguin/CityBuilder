3a97dca4f3400ca17ad7dbbdc5c1a0576abc52a4
Rendre <rendre.greyling@gmail.com>
2023-03-28 21:23:54 +0200

    Add arrays back to main class for testing

--- src/main/java/engine/engineMain/MainApp.java
+++ src/main/java/engine/engineMain/MainApp.java
@@ -2,6 +2,7 @@ package engine.engineMain;
 
 import engine.display.DisplayManager;
 import engine.models.RawModel;
+import engine.models.TexturedModel;
 import engine.renderEngine.Loader;
 import engine.renderEngine.MasterRenderer;
 
@@ -18,8 +19,16 @@ public class MainApp {
                 -0.5f, -0.5f,0f,
                 0.5f, -0.5f, 0f,
                 0.5f, 0.5f, 0f
+        };
 
+        int[] indices = {
+                0,1,3,
+                3,1,2
+        };
 
+        float[] textureCoords = {};
+
+        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
 
         MasterRenderer renderer = new MasterRenderer();
 
