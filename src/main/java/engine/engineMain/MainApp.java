6369ec89aa7edfd090f61599f06f8989b99a9684
Rendre <rendre.greyling@gmail.com>
2023-03-30 21:46:32 +0200

    Fix movement bugs with camera

--- src/main/java/engine/engineMain/MainApp.java
+++ src/main/java/engine/engineMain/MainApp.java
@@ -6,6 +6,8 @@ import engine.entities.Entity;
 import engine.entities.Light;
 import engine.models.RawModel;
 import engine.models.TexturedModel;
+import engine.objConverter.ModelData;
+import engine.objConverter.OBJFileLoader;
 import engine.renderEngine.Loader;
 import engine.renderEngine.MasterRenderer;
 import engine.terrain.Terrain;
@@ -20,106 +22,9 @@ public class MainApp {
         DisplayManager.createDisplay();
         Loader loader = new Loader();
 
-        /*float[] vertices = {
-                -0.5f, 0.5f, 0f,
-                -0.5f, -0.5f,0f,
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f
-        };
+        ModelData data = OBJFileLoader.loadOBJ("cube");
 
-        int[] indices = {
-                0,1,3,
-                3,1,2
-        };
-
-        float[] textureCoords = {
-                0,0,
-                0,1,
-                1,1,
-                1,0
-        };*/
-
-        float[] vertices = {
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,-0.5f,0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                0.5f,0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                -0.5f,-0.5f,0.5f,
-                -0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,-0.5f,0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f
-
-        };
-
-        float[] textureCoords = {
-
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
-
-
-        };
-
-        int[] indices = {
-                0,1,3,
-                3,1,2,
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
-
-        };
-        float[] normals ={};
-
-        RawModel model = loader.loadToVAO(vertices, textureCoords,normals ,indices);
+        RawModel model = loader.loadToVAO(data.getVertices(), data.getTextureCoords(), data.getNormals(), data.getIndices());
         TextureAttribute texture = new TextureAttribute(loader.loadTexture("spiral"));
         TexturedModel texturedModel = new TexturedModel(model, texture);
 
