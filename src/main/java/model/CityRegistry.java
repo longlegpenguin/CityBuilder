090dd1c28da51f3b47e2fb91abd8848d23bc21e4
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-03-31 22:29:47 +0200

    Added most of the methods to CityRegistry class.

--- src/main/java/model/CityRegistry.java
+++ src/main/java/model/CityRegistry.java
@@ -11,25 +11,48 @@ public class CityRegistry {
     private ArrayList<Facility> facilities;
     private ArrayList<Zone> zones;
     private LinkedList<Double> taxRatePast20Years;
-    public CityRegistry(){
+
+    public CityRegistry() {
         this.facilities = new ArrayList<>();
         this.zones = new ArrayList<>();
         this.taxRatePast20Years = new LinkedList<>();
     }
-    public double getPast20AvgIncome(){
+
+    public double getPast20AvgIncome() {
         double sumTax = 0;
         int cnt = 0;
-        for (int i = 0; i < taxRatePast20Years.size(); i++) {
-            sumTax += taxRatePast20Years.get(i);
+        for (Double taxRatePast20Year : taxRatePast20Years) {
+            sumTax += taxRatePast20Year;
             cnt += 1;
         }
         return sumTax / cnt;
     }
-    public void registerCitizenToZone(Citizen c){
 
+    public void registerCitizenToZone(Citizen citizen, Zone zone) {
+        zone.addCitizen(citizen);
     }
-    public void deregisterCitizen(Citizen c){}
-    public void updateOnConstruction(){}
-    public void addZone(Zone zone){}
-    public void addFacility(Facility facility){}
+
+    public void deregisterCitizen(Citizen citizen, Zone zone) {
+        zone.unregisterCitizen(citizen);
+    }
+
+    public void updateOnConstruction() {
+    }
+
+    public void addTaxRate(double newTax){
+        if (taxRatePast20Years.size() == 20) {
+            taxRatePast20Years.addLast(newTax);
+            taxRatePast20Years.removeFirst();
+        } else {
+            taxRatePast20Years.addLast(newTax);
+        }
+    }
+
+    public void addZone(Zone zone) {
+        zones.add(zone);
+    }
+
+    public void addFacility(Facility facility) {
+        facilities.add(facility);
+    }
 }
