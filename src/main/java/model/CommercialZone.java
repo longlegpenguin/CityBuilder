682a0487a9c1d78a9a9f26f21e50fb090a90ef75
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-03-31 19:47:44 +0200

    Added Zone children classes.

--- src/main/java/model/CommercialZone.java
+++ src/main/java/model/CommercialZone.java
@@ -1,4 +1,18 @@
 package model;
 
-public class CommercialZone {
+import model.util.BuildableType;
+import model.util.Level;
+import model.zone.Zone;
+
+public class CommercialZone extends Zone{
+
+    public CommercialZone(Level level, int dayToBuild, Statistics statistics, Date birthday) {
+        super(level, dayToBuild, statistics, birthday);
+    }
+
+    @Override
+    public BuildableType getBuildableType(){
+        return BuildableType.COMMERCIAL;
+    }
+
 }
--- src/main/java/model/IndustrialZone.java
+++ src/main/java/model/IndustrialZone.java
@@ -1,4 +1,18 @@
 package model;
 
-public class IndustrialZone {
+import model.util.BuildableType;
+import model.util.Level;
+import model.zone.Zone;
+
+public class IndustrialZone extends Zone{
+
+    public IndustrialZone(Level level, int dayToBuild, Statistics statistics, Date birthday) {
+        super(level, dayToBuild, statistics, birthday);
+    }
+
+    @Override
+    public BuildableType getBuildableType(){
+        return BuildableType.INDUSTRIAL;
+    }
+
 }
