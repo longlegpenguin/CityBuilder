19be1f36ea10a5e62f844bc5af28d55278b60c5c
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-03-31 21:22:51 +0200

    Added class Road.

--- src/main/java/model/Road.java
+++ src/main/java/model/Road.java
@@ -1,4 +1,18 @@
 package model;
 
-public class Road {
+import model.facility.Facility;
+import model.util.BuildableType;
+import model.util.Coordinate;
+import model.util.Dimension;
+
+public class Road extends Facility {
+
+    public Road(int oneTimeCost, int maintenanceFee, Coordinate coordinate, Dimension dimension) {
+        super(oneTimeCost, maintenanceFee, coordinate, dimension);
+    }
+
+    @Override
+    public BuildableType getBuildableType() {
+        return BuildableType.ROAD;
+    }
 }
