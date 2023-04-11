b114d6877ee8fff244dddef6e514a0792eacb63d
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-04-11 22:11:01 +0200

    Made ResidentialZoneFactory follow the same format as the other factories.

--- src/main/java/model/zone/ResidentialZoneFactory.java
+++ src/main/java/model/zone/ResidentialZoneFactory.java
@@ -1,11 +1,12 @@
 package model.zone;
 
-import model.*;
+import model.GameModel;
 import model.common.Coordinate;
-import model.util.Level;
 import model.common.Satisfaction;
+import model.util.Level;
 
 public class ResidentialZoneFactory extends ZoneFactory {
+
     public ResidentialZoneFactory(GameModel gm) {
         super(gm);
     }
@@ -15,7 +16,7 @@ public class ResidentialZoneFactory extends ZoneFactory {
         return new ResidentialZone(
                 Level.CONSTRUCTING,
                 1,
-                new ZoneStatistics(0,Level.CONSTRUCTING.getCapacity(), new Satisfaction()),
+                new ZoneStatistics(0, Level.CONSTRUCTING.getCapacity(), new Satisfaction()),
                 gm.getCurrentDate(),
                 coordinate
         );
