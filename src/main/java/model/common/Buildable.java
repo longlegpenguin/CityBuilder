de610462b27f640e1311675c03a01f2aa5f64e09
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-04-11 23:17:23 +0200

    Code cleanup (removed redundant public from methods)

--- src/main/java/model/common/Buildable.java
+++ src/main/java/model/common/Buildable.java
@@ -1,29 +1,27 @@
 package model.common;
 
 import model.util.BuildableType;
-import model.common.Coordinate;
-import model.common.Dimension;
 
 public interface Buildable {
     /**
      * @return the coordinate of the buildable
      */
-    public Coordinate getCoordinate();
+    Coordinate getCoordinate();
 
     /**
      * @return the dimension of the buildable
      */
-    public Dimension getDimension();
+    Dimension getDimension();
 
     /**
      * A type can be:
      * @return the type of the buildable
      */
-    public BuildableType getBuildableType();
+    BuildableType getBuildableType();
 
     /**
      * Every buildable cost some when built.
      * @return the costs.
      */
-    public int getConstructionCost();
+    int getConstructionCost();
 }
