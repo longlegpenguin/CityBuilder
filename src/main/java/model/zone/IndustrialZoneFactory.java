9b41d6d9fd2db8e91068446ead01470e4c767ca5
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-04-11 22:08:46 +0200

    Added CommercialZoneFactory and IndustrialZoneFactory.

---
+++ src/main/java/model/zone/IndustrialZoneFactory.java
@@ -0,0 +1,24 @@
+package model.zone;
+
+import model.GameModel;
+import model.common.Coordinate;
+import model.common.Satisfaction;
+import model.util.Level;
+
+public class IndustrialZoneFactory extends ZoneFactory {
+
+    public IndustrialZoneFactory(GameModel gm) {
+        super(gm);
+    }
+
+    @Override
+    public Zone createZone(Coordinate coordinate) {
+        return new IndustrialZone(
+                Level.CONSTRUCTING,
+                1,
+                new ZoneStatistics(0, Level.CONSTRUCTING.getCapacity(), new Satisfaction()),
+                gm.getCurrentDate(),
+                coordinate
+        );
+    }
+}
