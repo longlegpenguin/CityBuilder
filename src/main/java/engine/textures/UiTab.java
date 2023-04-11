6289a7c3bdc2936c55a114da342832c23f99a1e9
Abdelaziz Dhaouadi <azizdh0102@gmail.com>
2023-04-11 22:59:20 +0200

    There is a button on the display that is working

---
+++ src/main/java/engine/textures/UiTab.java
@@ -0,0 +1,12 @@
+package engine.textures;
+
+import engine.guis.UiComponent;
+import org.joml.Vector2f;
+
+public class UiTab extends UiComponent {
+
+    public UiTab(int texture, Vector2f position, Vector2f scale) {
+        super(texture, position, scale);
+        this.isClickable = false;
+    }
+}
