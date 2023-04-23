a436c3438e52c4ad0ec3ed8993a4345d7ed03dea
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-04-23 20:42:42 +0200

    Removed everything regarding budget and tax from here, moved it to CityStatistics and Budget classes.

--- src/main/java/model/city/CityRegistry.java
+++ src/main/java/model/city/CityRegistry.java
@@ -1,39 +1,21 @@
 package model.city;
 
-import model.common.Budget;
 import model.common.Citizen;
 import model.facility.Facility;
 import model.zone.Zone;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class CityRegistry {
 
     private ArrayList<Facility> facilities;
     private ArrayList<Zone> zones;
-    private LinkedList<Double> taxRatePast20Years;
-    private CityStatistics cityStatistics; // TODO (I need it to be here for game model)
+    private CityStatistics cityStatistics;
 
-    public CityRegistry() {
+    public CityRegistry(CityStatistics cityStatistics) {
         this.facilities = new ArrayList<>();
         this.zones = new ArrayList<>();
-        this.taxRatePast20Years = new LinkedList<>();
-        this.cityStatistics = new CityStatistics((float) 0.3); // TODO ??
-    }
-
-    public CityStatistics getCityStatistics() {
-        return cityStatistics;
-    }
-
-    public double getPast20AvgIncome() {
-        double sumTax = 0;
-        int cnt = 0;
-        for (Double taxRatePast20Year : taxRatePast20Years) {
-            sumTax += taxRatePast20Year;
-            cnt += 1;
-        }
-        return sumTax / cnt;
+        this.cityStatistics = cityStatistics;
     }
 
     public ArrayList<Facility> getFacilities() {
@@ -44,21 +26,16 @@ public class CityRegistry {
         return zones;
     }
 
+    public CityStatistics getCityStatistics() {
+        return cityStatistics;
+    }
+
     public void registerCitizenToZone(Citizen citizen, Zone zone) {
         zone.addCitizen(citizen);
     }
 
     public void deregisterCitizen(Citizen citizen, Zone zone) {
         zone.unregisterCitizen(citizen);
-    }
-
-    public void addTaxRate(double newTax) {
-        if (taxRatePast20Years.size() == 20) {
-            taxRatePast20Years.addLast(newTax);
-            taxRatePast20Years.removeFirst();
-        } else {
-            taxRatePast20Years.addLast(newTax);
-        }
     }
 
     public void addZone(Zone zone) {
@@ -79,23 +56,4 @@ public class CityRegistry {
         facilities.remove(facility);
     }
 
-    // TODO
-    public void deductBalance(int amount) {
-    }
-
-    // TODO
-    public void addMaintenanceFee(int amount) {
-    }
-
-    // TODO
-    public void addBalance(double v) {
-    }
-
-    //TODO
-    public Budget getBudget() {
-        return null;
-    }
-    // TODO
-    public void deductMaintenanceFee(int maintenanceFee) {
-    }
 }
