17012d0f1cf464d581b26a6da651a5b17a754837
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-04-23 22:15:28 +0200

    Created LevelOfEducation enum, and added it as a constructor parameter to Education.

--- src/main/java/model/facility/Education.java
+++ src/main/java/model/facility/Education.java
@@ -2,16 +2,21 @@ package model.facility;
 
 import model.common.Coordinate;
 import model.common.Dimension;
+import model.util.LevelOfEducation;
 
 abstract class Education extends Facility{
 
     protected int capacity;
     protected int yearsToGraduate;
 
-    public Education(int oneTimeCost, int maintenanceFee, Coordinate coordinate, Dimension dimension, int capacity, int yearsToGraduate) {
+    protected LevelOfEducation levelOfEducation;
+
+    public Education(int oneTimeCost, int maintenanceFee, Coordinate coordinate, Dimension dimension, int capacity,
+                     int yearsToGraduate, LevelOfEducation levelOfEducation) {
         super(oneTimeCost, maintenanceFee, coordinate, dimension);
         this.capacity = capacity;
         this.yearsToGraduate = yearsToGraduate;
+        this.levelOfEducation = levelOfEducation;
     }
 
     public int getCapacity() {
