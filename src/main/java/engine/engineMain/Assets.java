27f0f4babc7485e2668d7769105d596261dbb6ba
Rendre <rendre.greyling@gmail.com>
2023-04-25 11:02:35 +0200

    Commenting of Assets Enum

--- src/main/java/engine/engineMain/Assets.java
+++ src/main/java/engine/engineMain/Assets.java
@@ -1,5 +1,9 @@
 package engine.engineMain;
 
+/**
+ * Enum used to store the necesarry data to load the files into the Texture processing and VAO's.
+ * Stores the scale of each object, as well as the .obj filename and .png texture filename.
+ */
 public enum Assets {
     ROAD ("road", "road", 5),
     RESEDENTIAL("cube", "spiral", 5),
@@ -15,20 +19,35 @@ public enum Assets {
     private String TextureFileName;
     private float scale;
 
+    /**
+     * Constructor of enum.
+     * @param OBJFileName
+     * @param TextureFileName
+     * @param scale
+     */
     Assets(String OBJFileName, String TextureFileName, float scale) {
         this.OBJFilename = OBJFileName;
         this.TextureFileName = TextureFileName;
         this.scale = scale;
     }
 
+    /**
+     * @return .obj filename as a string.
+     */
     public String getOBJFilename() {
         return OBJFilename;
     }
 
+    /**
+     * @return .png filename as a string.
+     */
     public String getTextureFileName() {
         return TextureFileName;
     }
 
+    /**
+     * @return scale of object as int
+     */
     public float getScale() {
         return scale;
     }
