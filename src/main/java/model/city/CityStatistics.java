3338d5db2b44b637f41a54962c626a618edfcd04
Ala Tarlajanu <alatarlajanu@gmail.com>
2023-05-01 01:11:06 +0200

    Added factors which impact the whole's city satisfaction as attributes(deleted them from Satisfaction class, renaming Satisfaction class => ZoneSatisfaction, and leaving there only the attributes which impact the zone specifically). Changed the logic of getting city's satisfaction. Added Javadocs.

--- src/main/java/model/city/CityStatistics.java
+++ src/main/java/model/city/CityStatistics.java
@@ -1,6 +1,9 @@
 package model.city;
 
+import model.GameModel;
 import model.common.Budget;
+import model.common.Citizen;
+import model.util.LevelOfEducation;
 import model.zone.CommercialZone;
 import model.zone.IndustrialZone;
 import model.zone.ResidentialZone;
@@ -11,19 +14,20 @@ public class CityStatistics {
     private int nrIndustrialZones;
     private int nrResidentialZones;
     private final Budget budget;
-    private int population;
-    private float citizenSatisfaction;
+    private float citySatisfaction;
+    private int taxEffect;
+    private double indComZoneBalance;
+    private float budgetEffect;
 
     public CityStatistics(Budget budget) {
         this.budget = budget;
         this.nrCommercialZones = 0;
         this.nrIndustrialZones = 0;
         this.nrResidentialZones = 0;
-        this.citizenSatisfaction = 0;
-    }
-
-    public Budget getBudget() {
-        return budget;
+        this.taxEffect = 0;
+        this.indComZoneBalance = 1;
+        this.budgetEffect = 0;
+        this.citySatisfaction = 0;
     }
 
     public int getNrCommercialZones() {
@@ -38,6 +42,16 @@ public class CityStatistics {
         return nrResidentialZones;
     }
 
+    public Budget getBudget() {
+        return budget;
+    }
+
+    /**
+     * Get city population.
+     *
+     * @param cityRegistry used to get the zones
+     * @return number of citizens.
+     */
     public int getPopulation(CityRegistry cityRegistry) {
         int populationCnt = 0;
         for (Zone z : cityRegistry.getZones()) {
@@ -46,17 +60,40 @@ public class CityStatistics {
         return populationCnt;
     }
 
-    public void setCitizenSatisfaction(CityRegistry cityRegistry) {
-        float totalCitizenSatisf = 0;
-        int nrZones = 0;
-        for (Zone z : cityRegistry.getZones()) {
-            totalCitizenSatisf += z.getStatistics().getSatisfaction().getTotalSatisfaction();
-            nrZones += 1;
-        }
-
-        this.citizenSatisfaction = totalCitizenSatisf / nrZones;
+    public float getCitySatisfaction() {
+        return citySatisfaction;
     }
 
+    /**
+     * Setting the indComZoneBalance to represent the balance between industrial and commercial buildings.
+     *
+     * @param indComZoneBalance balance between industrial and commercial buildings
+     */
+    public void setZoneBalanceEffect(double indComZoneBalance) {
+        this.indComZoneBalance = indComZoneBalance;
+    }
+
+    /**
+     * Setting city average satisfaction being equal to the sum of the average satisfaction of zones
+     * and of the average of some additional effects which are related to the whole city (avgCommonZoneSatisfaction).
+     *
+     * @param gm used to get the cityRegistry
+     */
+    public void setCitySatisfaction(GameModel gm) {
+        int sumZoneSatisfaction = 0;
+        for (Zone zone : gm.getCityRegistry().getZones()) {
+            sumZoneSatisfaction += zone.getSatisfaction();
+        }
+        float avgZonesSatisfaction = (float) sumZoneSatisfaction / gm.getCityRegistry().getZones().size();
+        float avgCommonZoneSatisfaction = (float) (taxEffect + indComZoneBalance + budgetEffect) / 3;
+        this.citySatisfaction = avgZonesSatisfaction + avgCommonZoneSatisfaction;
+    }
+
+    /**
+     * Update the number of zones of each type.
+     *
+     * @param cityRegistry to get the zones
+     */
     public void updateNrZones(CityRegistry cityRegistry) {
         this.nrCommercialZones = (int) cityRegistry.getZones().stream().filter(z -> z.getClass()
                 .equals(CommercialZone.class)).count();
@@ -66,16 +103,32 @@ public class CityStatistics {
                 .equals(ResidentialZone.class)).count();
     }
 
-    public void setNrCommercialZones(int nr) {
-        this.nrCommercialZones = nr;
+    /**
+     * Get the number of citizens who have a secondary level of education (school).
+     *
+     * @param cityRegistry to get the collection of citizens.
+     * @return nr of citizens who have a secondary level of education (school).
+     */
+    public int getNrCitizenSecondaryEducation(CityRegistry cityRegistry) {
+        int nrCitizenSecondaryEducation = 0;
+        for (Citizen citizen : cityRegistry.getAllCitizens()) {
+            if (citizen.getLevelOfEducation() == LevelOfEducation.SCHOOL) nrCitizenSecondaryEducation += 1;
+        }
+        return nrCitizenSecondaryEducation;
     }
 
-    public void setNrIndustrialZonesZones(int nr) {
-        this.nrIndustrialZones = nr;
-    }
-
-    public void setNrResidentialZones(int nr) {
-        this.nrResidentialZones = nr;
+    /**
+     * Get the number of citizens who have a higher level of education (university).
+     *
+     * @param cityRegistry to get the collection of citizens.
+     * @return nr of citizens who have a higher level of education (university).
+     */
+    public int getNrCitizenHigherEducation(CityRegistry cityRegistry) {
+        int nrCitizenHigherEducation = 0;
+        for (Citizen citizen : cityRegistry.getAllCitizens()) {
+            if (citizen.getLevelOfEducation() == LevelOfEducation.UNIVERSITY) nrCitizenHigherEducation += 1;
+        }
+        return nrCitizenHigherEducation;
     }
 
 }
