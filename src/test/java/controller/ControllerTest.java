7bb7bbe3cc872e704bef735951914cf2f9ce0fe8
Zhang Li <73643920+longlegpenguin@users.noreply.github.com>
2023-05-04 09:12:10 +0200

    Add controller time mode unit test.

--- src/test/java/controller/ControllerTest.java
+++ src/test/java/controller/ControllerTest.java
@@ -1,8 +1,10 @@
 package controller;
 
 import controller.util.GameMode;
+import controller.util.TimeMode;
 import model.GameModel;
 import model.common.Coordinate;
+import model.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,5 +27,32 @@ class ControllerTest {
         controller.mouseClickRequest(new Coordinate(1, 1), null);
         System.out.println(gm.printMap());
         assertEquals(gm.getAllBuildable().size(), 2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTimeModeDaily() {
+        controller.switchTimeModeRequest(TimeMode.DAILY);
+        Date before = gm.getCurrentDate();
+        controller.regularUpdateRequest(1, null);
+        Date after = gm.getCurrentDate();
+        assertEquals(1, after.dateDifference(before).get("days"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTimeModeWeekly() {
+        controller.switchTimeModeRequest(TimeMode.WEEKLY);
+        Date before = gm.getCurrentDate();
+        controller.regularUpdateRequest(1, null);
+        Date after = gm.getCurrentDate();
+        assertEquals(7, after.dateDifference(before).get("days"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTimeModeMonthly() {
+        controller.switchTimeModeRequest(TimeMode.MONTHLY);
+        Date before = gm.getCurrentDate();
+        controller.regularUpdateRequest(1, null);
+        Date after = gm.getCurrentDate();
+        assertEquals(30, after.dateDifference(before).get("days"));
     }
 }
