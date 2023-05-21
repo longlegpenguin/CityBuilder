498f02d502a7aec7a6f9fe0baf5f3d8109cc73b9
Rendre <rendre.greyling@gmail.com>
2023-05-21 22:25:47 +0200

    Completion of documentation of TerrainShader

--- src/main/java/engine/shaders/TerrainShader.java
+++ src/main/java/engine/shaders/TerrainShader.java
@@ -6,6 +6,9 @@ import engine.tools.Maths;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
+/**
+ * Extends the ShaderProgram and is responsible for handling the shaders for all terrain objects.
+ */
 public class TerrainShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "src/main/java/engine/shaders/terrainVertexShader.glsl";
@@ -25,6 +28,9 @@ public class TerrainShader extends ShaderProgram {
         super(VERTEX_FILE, FRAGMENT_FILE);
     }
 
+    /**
+     * Binds all Attributes found in the GLSL file.
+     */
     @Override
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
@@ -32,6 +38,9 @@ public class TerrainShader extends ShaderProgram {
         super.bindAttribute(2, "normal");
     }
 
+    /**
+     * Finds all uniform loactions in the GLSL file.
+     */
     @Override
     protected void getAllUniformLocations() {
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
@@ -44,29 +53,56 @@ public class TerrainShader extends ShaderProgram {
         location_skyColor = super.getUniformLocation("skyColor");
     }
 
+    /**
+     * Loads the sky color to the shader - based on colors set in master renderer.
+     * @param r
+     * @param g
+     * @param b
+     */
     public void loadSkyColor(float r, float g, float b) {
         super.loadVector(location_skyColor, new Vector3f(r, g, b));
     }
 
+    /**
+     * Loads the shine and reflection variables to the shader.
+     * @param shineDamper
+     * @param reflectivity
+     */
     public void loadShineVariables(float shineDamper, float reflectivity) {
         super.loadFloat(location_shineDamper, shineDamper);
         super.loadFloat(location_reflectivity, reflectivity);
     }
 
+    /**
+     * Loads the transformation matrix to the shader.
+     * @param matrix
+     */
     public void loadTransformationMatrix(Matrix4f matrix) {
         super.loadMatrix(location_transformationMatrix, matrix);
     }
 
+    /**
+     * Loads the light to the shader - mainly its position and color.
+     * @param light
+     */
     public void loadLight(Light light) {
         super.loadVector(location_lightPosition, light.getPosition());
         super.loadVector(location_lightColor, light.getColor());
     }
 
+    /**
+     * Loads the view matrix to the shader after creating it based on the camera.
+     * @param camera
+     */
     public void loadViewMatrix(Camera camera) {
         Matrix4f viewMatrix = Maths.createViewMatrix(camera);
         super.loadMatrix(location_viewMatrix, viewMatrix);
     }
 
+    /**
+     * Loads the projection matrix to the shader.
+     * @param projection
+     */
     public void loadProjectionMatrix(Matrix4f projection) {
         super.loadMatrix(location_projectionMatrix, projection);
     }
