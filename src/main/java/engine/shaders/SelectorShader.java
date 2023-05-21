b068c4b89bf9423cea1ffcbadd3b75be15d78c08
Rendre <rendre.greyling@gmail.com>
2023-05-21 22:24:01 +0200

    Completion of documentation of SelectorShader

--- src/main/java/engine/shaders/SelectorShader.java
+++ src/main/java/engine/shaders/SelectorShader.java
@@ -4,6 +4,9 @@ import engine.entities.Camera;
 import engine.tools.Maths;
 import org.joml.Matrix4f;
 
+/**
+ * Extends the ShaderProgram and is responsible for handling the shaders for the selector square used for selecting a zone.
+ */
 public class SelectorShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "src/main/java/engine/shaders/SelectorVertexShader.glsl";
@@ -16,6 +19,9 @@ public class SelectorShader extends ShaderProgram {
         super(VERTEX_FILE, FRAGMENT_FILE);
     }
 
+    /**
+     * Binds all Attributes found in the GLSL file.
+     */
     @Override
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
@@ -23,6 +29,9 @@ public class SelectorShader extends ShaderProgram {
         super.bindAttribute(2, "normal");
     }
 
+    /**
+     * Finds all uniform loactions in the GLSL file.
+     */
     @Override
     protected void getAllUniformLocations() {
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
@@ -30,15 +39,27 @@ public class SelectorShader extends ShaderProgram {
         location_viewMatrix = super.getUniformLocation("viewMatrix");
     }
 
+    /**
+     * Loads the transformation matrix to the shader.
+     * @param matrix
+     */
     public void loadTransformationMatrix(Matrix4f matrix) {
         super.loadMatrix(location_transformationMatrix, matrix);
     }
 
+    /**
+     * Loads the view matrix to the shader after creating it based on the camera.
+     * @param camera
+     */
     public void loadViewMatrix(Camera camera) {
         Matrix4f viewMatrix = Maths.createViewMatrix(camera);
         super.loadMatrix(location_viewMatrix, viewMatrix);
     }
 
+    /**
+     * Loads the projection matrix to the shader.
+     * @param projection
+     */
     public void loadProjectionMatrix(Matrix4f projection) {
         super.loadMatrix(location_projectionMatrix, projection);
     }
