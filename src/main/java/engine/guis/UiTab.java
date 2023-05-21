e1f9d367047909361feb560105ca8d8ba0d7ae3a
Rendre <rendre.greyling@gmail.com>
2023-05-21 17:44:19 +0200

    Completion of documentation for UITab

--- src/main/java/engine/guis/UiTab.java
+++ src/main/java/engine/guis/UiTab.java
@@ -2,6 +2,11 @@ package engine.guis;
 
 import org.joml.Vector2f;
 
+/**
+ * Extension of the UI Component
+ * Non-clickable tab which is a rectangular object with Texture.
+ * Used as back plane for for menu's
+ */
 public class UiTab extends UiComponent {
 
     public UiTab(int texture, Vector2f position, Vector2f scale) {
