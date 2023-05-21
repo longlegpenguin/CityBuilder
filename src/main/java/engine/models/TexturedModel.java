2b1a136a5a10a67fc196d3a2def15c17f2daace0
Rendre <rendre.greyling@gmail.com>
2023-05-21 17:57:23 +0200

    Completion of documentation in RawModel and TexturedModel

--- src/main/java/engine/models/TexturedModel.java
+++ src/main/java/engine/models/TexturedModel.java
@@ -11,15 +11,25 @@ public class TexturedModel {
     private RawModel rawModel;
     private TextureAttribute texture;
 
+    /**
+     * @param rawModel
+     * @param texture
+     */
     public TexturedModel(RawModel rawModel, TextureAttribute texture) {
         this.rawModel = rawModel;
         this.texture = texture;
     }
 
+    /**
+     * @return RawModel object of Asset.
+     */
     public RawModel getRawModel() {
         return rawModel;
     }
 
+    /**
+     * @return The texture of the model as Texture Attribute object.
+     */
     public TextureAttribute getTexture() {
         return texture;
     }
