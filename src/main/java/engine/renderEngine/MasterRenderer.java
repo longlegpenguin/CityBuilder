962f08e5843673c10d3cae45769db71a6d1c7f48
Rendre <rendre.greyling@gmail.com>
2023-05-21 21:58:21 +0200

    Completion of documentation of MasterRenderer

--- src/main/java/engine/renderEngine/MasterRenderer.java
+++ src/main/java/engine/renderEngine/MasterRenderer.java
@@ -20,6 +20,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The main renderer class which starts all individual renderers and prepares the array of objects for each.
+ */
 public class MasterRenderer {
 
     private static final float FOV = 70;
@@ -44,6 +47,9 @@ public class MasterRenderer {
     private SelectorRenderer selectorRenderer;
 
 
+    /**
+     * Sets OPENGL Properties and starts all the renderers.
+     */
     public MasterRenderer() {
         GL11.glEnable(GL11.GL_CULL_FACE);
         GL11.glCullFace(GL11.GL_BACK);
@@ -53,6 +59,12 @@ public class MasterRenderer {
         selectorRenderer = new SelectorRenderer(selectorShader, projectionMatrix);
     }
 
+    /**
+     * Calls the render function of each renderer as well as loading the camera position and light to each shader.
+     * @param selector
+     * @param camera
+     * @param light
+     */
     public void render(Selector selector, Camera camera, Light light) {
         prepare();
         entityShader.start();
@@ -77,6 +89,10 @@ public class MasterRenderer {
         selectorShader.stop();
     }
 
+    /**
+     * Processes each entity into the entity HashMap
+     * @param entity
+     */
     public void processEntities(Entity entity) {
         TexturedModel texturedModel = entity.getModel();
         List<Entity> batch = entities.get(texturedModel);
@@ -89,6 +105,10 @@ public class MasterRenderer {
         }
     }
 
+    /**
+     * Processes each terrain into the terrain hashmap
+     * @param terrain
+     */
     public void processTerrain(Terrain terrain) {
         TextureAttribute texture = terrain.getTexture();
         List<Terrain> batch = terrains.get(texture);
@@ -101,22 +121,34 @@ public class MasterRenderer {
         }
     }
 
+    /**
+     * Sets the OPENGL properties before rendering.
+     */
     public void prepare() {
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         GL11.glClearColor(RED,GREEN,BLUE, 1);
     }
 
+    /**
+     * Calls the cleanup methods for each shader.
+     */
     public void cleanUp() {
         entityShader.cleanUp();
         terrainShader.cleanUp();
         selectorShader.cleanUp();
     }
 
+    /**
+     * @return the current projection matrix based on the screen
+     */
     public Matrix4f getProjectionMatrix() {
         return  projectionMatrix;
     }
 
+    /**
+     * Creates the projection matrix based on the screen size.
+     */
     private void createProjectionMatrix() {
         projectionMatrix = new Matrix4f().perspective(
                 (float) Math.toRadians(FOV),
