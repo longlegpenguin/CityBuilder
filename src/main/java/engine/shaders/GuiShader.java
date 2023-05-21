1874431db6639a922da5133bac465211b66581e5
Rendre <rendre.greyling@gmail.com>
2023-05-21 22:22:46 +0200

    Completion of documentation of GuiShader

--- src/main/java/engine/shaders/GuiShader.java
+++ src/main/java/engine/shaders/GuiShader.java
@@ -1,8 +1,10 @@
 package engine.shaders;
 
-import engine.shaders.ShaderProgram;
 import org.joml.Matrix4f;
 
+/**
+ * Extends the ShaderProgram and is responsible for handling the shaders for all 2D UI components.
+ */
 public class GuiShader extends ShaderProgram {
     private static final String VERTEX_FILE ="src/main/java/engine/shaders/guiVertexShader.glsl";
     private static final String FRAGMENT_FILE="src/main/java/engine/shaders/guiFragmentShader.glsl";
@@ -12,16 +14,27 @@ public class GuiShader extends ShaderProgram {
     public GuiShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
     }
+
+    /**
+     * Loads the transformation matrix to the shader.
+     * @param matrix
+     */
     public void loadTransformation(Matrix4f matrix)
     {
         super.loadMatrix(location_transformationMatrix,matrix);
     }
 
+    /**
+     * Binds all Attributes found in the GLSL file.
+     */
     @Override
     protected void bindAttributes() {
         super.bindAttribute(0,"position");
     }
 
+    /**
+     * Finds all uniform loactions in the GLSL file.
+     */
     @Override
     protected void getAllUniformLocations() {
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
