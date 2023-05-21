83a2495a950a023df7d6190122efcf7a9c7db3f2
Rendre <rendre.greyling@gmail.com>
2023-05-21 22:37:27 +0200

    Completion of documentation of Keyboard

--- src/main/java/engine/tools/Keyboard.java
+++ src/main/java/engine/tools/Keyboard.java
@@ -4,6 +4,10 @@ import org.lwjgl.glfw.GLFWKeyCallback;
 
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 
+/**
+ * Keyboard class to detect when a key is pressed.
+ * Uses the GLFWKeyCallBack to detect.
+ */
 public class Keyboard extends GLFWKeyCallback {
     private static boolean[] keys = new boolean[65536];
 
@@ -15,6 +19,11 @@ public class Keyboard extends GLFWKeyCallback {
         keys[key] = action != GLFW_RELEASE;
     }
 
+    /**
+     * Returns whether a key has been pressed.
+     * @param keycode
+     * @return
+     */
     public static boolean isKeyDown(int keycode) {
         return keys[keycode];
     }
