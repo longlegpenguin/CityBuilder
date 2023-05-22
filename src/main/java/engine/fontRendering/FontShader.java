81bf3973f895a479a3511c4b91a2c132b517519d
Abdelaziz Dhaouadi <azizdh0102@gmail.com>
2023-05-22 17:18:17 +0200

    Documentation of the FontShader Class

--- src/main/java/engine/fontRendering/FontShader.java
+++ src/main/java/engine/fontRendering/FontShader.java
@@ -6,7 +6,9 @@ import org.joml.Vector2f;
 import org.joml.Vector3f;
 
 public class FontShader extends ShaderProgram {
-
+	/**
+	 * files of the vertex and the fragment (glsl files)
+	 */
 	private static final String VERTEX_FILE = "src/main/java/engine/fontRendering/fontVertex.glsl";
 	private static final String FRAGMENT_FILE = "src/main/java/engine/fontRendering/fontFragment.glsl";
 
@@ -18,12 +20,20 @@ public class FontShader extends ShaderProgram {
 
 	}
 
+	/**
+	 * method needed for the rendering to get all the uniforms (glsl file)
+	 */
+
 	@Override
 	protected void getAllUniformLocations() {
 		location_colour = super.getUniformLocation("colour");
 		location_translation = super.getUniformLocation("translation");
 		
 	}
+
+	/**
+	 * binding the attributes
+	 */
 
 	@Override
 	protected void bindAttributes() {
@@ -33,11 +43,21 @@ public class FontShader extends ShaderProgram {
 
 
 	}
+
+	/**
+	 * method to load the colour of the texts
+	 * @param colour
+	 */
 	protected  void loadColour(Vector3f colour)
 	{
 		super.loadVector(location_colour,colour);
 
 	}
+
+	/**
+	 * loads the translation of the text when rendered
+	 * @param translation
+	 */
 	protected  void loadTranslation(Vector2f translation)
 	{
 		super.load2DVector(location_translation,translation);
