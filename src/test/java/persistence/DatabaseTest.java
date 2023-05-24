d605071aaf79243255a53281c359b8fc656678df
Zhang Li <longlegpenguin@gmail.com>
2023-05-25 01:52:39 +0200

    Remove warnings

--- src/test/java/persistence/DatabaseTest.java
+++ src/test/java/persistence/DatabaseTest.java
@@ -1,16 +1,13 @@
 package persistence;
 
 import model.GameModel;
-import model.common.Coordinate;
-import model.exceptions.OperationException;
-import model.zone.ResidentialZoneFactory;
 import org.junit.jupiter.api.Test;
 
 public class DatabaseTest {
 
     @Test
-    public void runDatabaseMethods() throws OperationException {
-        GameModel gm = new GameModel(10,10);
+    public void runDatabaseMethods() {
+        GameModel gm = new GameModel(10, 10);
         gm.initialize();
         gm.regularUpdate(2, null);
         Database.save(gm);
